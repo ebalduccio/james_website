95f1b1d3130ffea24f3ecc2437968eb8462506d8
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2023-12-27 11:49:35 -0300

    Update InterestsCard.tsx

--- src/components/InterestsCard.tsx
+++ src/components/InterestsCard.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const InterestsCards = () => {
     return (
-        <div className="h-48 w-60 bg-stone-600 text-white font-roboto rounded-md"> {/* Reduced height and width */}
+        <div className="h-52 w-60 bg-stone-600 text-white font-roboto rounded-md"> {/* Reduced height and width */}
             <div className="mt-2 ml-2 px-1 py-1 flex items-center rounded-lg h-10 w-10 bg-gray-300"> {/* Adjusted spacing and size */}
                 <Image
                     src={Educationicon}
@@ -13,8 +13,8 @@ const InterestsCards = () => {
                 />
             </div>
             <div className="flex flex-col items-center gap-3 "> {/* Adjusted gap */}
-                <h1 className="text-2xl">Title</h1> {/* Smaller text */}
-                <p className="text-xs text-center max-w-xs px-3"> {/* Adjusted padding and font size */}
+                <h1 className="text-xl">Title</h1> {/* Smaller text */}
+                <p className="text-[0.7rem] text-center max-w-xs px-3"> {/* Adjusted padding and font size */}
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cupiditate mollitia delectus in officia illo veniam deserunt aut impedit,
                     voluptatibus assumenda eligendi atque. Commodi, quos nulla? Facilis praesentium nostrum perferendis.
                 </p>
