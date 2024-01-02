3e476bd28e4dc9e8c2ebb15d48c6b6870752fd21
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-01-02 12:55:37 -0300

    Create layout.tsx

---
+++ src/app/socialmedia/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next'
+import '../globals.css'
+import { cn } from '@/lib/utils'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+
+export const metadata = {
+    title: 'Social Media',
+    description: 'Social Media of James A. Kostohryz',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en" className='h-full '>
+            <body className={cn('relative h-full antialiased font-roboto')}>
+                <main className='absolute sm:relative flex flex-col min-h-screen'>
+                    <div className='flex-grow flex-1'>
+                        <Navbar />
+                        {children}
+                        <Footer />
+                    </div>
+                </main>
+            </body>
+        </html>
+    )
+}
