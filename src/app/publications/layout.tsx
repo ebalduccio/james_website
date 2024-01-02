b983cac31e9034cd555e773ef107a9e20f5b457a
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-01-02 12:55:41 -0300

    Update layout.tsx

--- src/app/publications/layout.tsx
+++ src/app/publications/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
 import '../globals.css'
 import { cn } from '@/lib/utils'
 import Navbar from '@/components/Navbar'
@@ -19,11 +18,11 @@ export default function RootLayout({
     <html lang="en" className='h-full '>
       <body className={cn('relative h-full antialiased font-roboto')}>
         <main className='absolute sm:relative flex flex-col min-h-screen'>
-          <Navbar />
           <div className='flex-grow flex-1'>
+            <Navbar />
             {children}
+            <Footer />
           </div>
-          <Footer />
         </main>
       </body>
     </html>
