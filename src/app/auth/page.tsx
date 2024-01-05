0eb8a4d3945a2805cbac78a393cf038db91ff902
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-01-05 09:13:19 -0300

    Create page.tsx

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,58 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import SectionTitle from "@/components/SectionTitle";
+
+import Image from "next/image";
+import authfb from '../../../public/AuthFacebook.svg'
+import authld from '../../../public/AuthLinkedin.svg'
+import { Button } from "@/components/ui/button";
+
+export default function AuthPage() {
+    return (
+        <>
+            <section className="h-[60rem] bg-whitebg bg-cover bg-center bg-no-repeat">
+                <MaxWidthWrapper>
+                    <SectionTitle TextColor="black" VectorColor="Black" title="James Kostohryz" subtitle="Authentication" />
+                    <div className="flex flex-col items-center mt-20">
+                        <div className="w-[30rem] sm:w-[40rem] h-[26rem] relative bg-stone-500 rounded-3xl">
+                            <div className="absolute right-24 bottom-[21.6rem]">
+                                <p className="text-blue-200">
+                                    What is Profile Id?
+                                </p>
+                            </div>
+                            <div className="flex flex-col gap-5 h-full items-center justify-center">
+                                <div className="flex gap-4 items-center">
+                                    <div>
+                                        <Image
+                                            src={authfb}
+                                            alt="auth fb icon"
+                                        />
+                                    </div>
+                                    <div>
+                                        <input type="text" className="h-10 w-[16rem] pl-2 rounded-md" />
+                                    </div>
+                                </div>
+                                <p className="text-white text-center text-lg">Or</p>
+                                <div className="flex items-center">
+                                    <div className="pr-2">
+                                        <Image
+                                            src={authld}
+                                            alt="auth fb icon"
+                                        />
+                                    </div>
+                                    <div className="">
+                                        <input type="text" className="h-10 w-[16rem] pl-2 rounded-md" />
+                                    </div>
+                                </div>
+                                <div className="pt-10">
+                                    <Button variant={"sucess"}>
+                                        Authenticate
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </MaxWidthWrapper>
+            </section >
+        </>
+    )
+}
