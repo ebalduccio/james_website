ed96ad22fbb1e0b35f3e33f6346b0710f40c0cd9
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-01-05 09:13:36 -0300

    Update SectionTitle.tsx

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -12,7 +12,7 @@ function SectionTitle({ title, subtitle, VectorColor, TextColor }: TitleProps) {
                     height={0}
                 />
             </div>
-            <div className='flex w-full items-center justify-center pt-3'>
+            <div className='flex w-full text-center items-center justify-center pt-3'>
                 <h1 className={`text-${TextColor} text-6xl font-normal font-lora`}>{title}</h1>
             </div>
             <div className='flex items-center justify-center pt-7'>
