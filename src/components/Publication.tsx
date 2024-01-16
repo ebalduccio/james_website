af2e9f83089779c0ca7593061125bea8a4f185ab
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-01-16 12:42:30 -0300

    Update Publication.tsx

--- src/components/Publication.tsx
+++ src/components/Publication.tsx
@@ -11,19 +11,21 @@ type PubProps = {
 
 function Publication({ title, link, date, Darkmode = true }: PubProps) {
     return (
-        <div className={cn('w-[25rem] sm:w-[35rem] lg:w-[45rem] h-48 rounded-sm mb-4', {
+        <div className={cn('w-[25rem] sm:w-[35rem] flex justify-center items-center lg:w-[45rem] h-48 rounded-sm mb-4', {
             'bg-neutral-500': Darkmode,
             'bg-white': !Darkmode
         })}>
-            <div className="mx-2 flex items-center gap-4">
-                <div className={cn('flex flex-col text-whites gap-4 mt-4 ml-4 text-left')}>
-                    <a href={link} target="_blank" rel="noopener noreferrer">
+            <a href={link} target="_blank" rel="noopener noreferrer">
+                <div className="mx-2 flex gap-4">
+                    <div className={cn('flex flex-col gap-4 mt-4 text-center', {
+                        'text-white': Darkmode,
+                    })}>
                         <h1 className="text-2xl">{title}</h1>
-                    </a>
-                    <p className="text-sm text-end pr-2">{new Date(date).toLocaleDateString()}</p>
-                </div>
-            </div>
-        </div >
+                        <p className="text-sm text-end pr-2">{new Date(date).toLocaleDateString()}</p>
+                    </div>
+                </div >
+            </a>
+        </div>
     )
 }
 
