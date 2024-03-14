d33a15b77c5501fd07e83a0a391a1b1370c8db37
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-03-13 21:56:27 -0300

    Update PublicationSocialMedia.tsx

--- src/components/PublicationSocialMedia.tsx
+++ src/components/PublicationSocialMedia.tsx
@@ -7,7 +7,7 @@ const PublicationSocialMedia = () => {
 
     return (
         <div>
-            <div className={cn('w-[25rem] sm:w-[35rem] lg:w-[45rem] hover:bg-neutral-400 duration-150 h-48 rounded-sm bg-neutral-500')}>
+            <div className={cn('w-[25rem] lg:w-[45rem] hover:bg-neutral-400 duration-150 h-48 rounded-sm bg-neutral-500')}>
                 <div className="mx-2 flex items-center gap-4">
                     <div className={cn('flex flex-col relative gap-4 mt-4 ml-4 text-left text-white')}>
                         <div className='absolute right-0'>
@@ -17,7 +17,7 @@ const PublicationSocialMedia = () => {
                             />
                         </div>
                         <h1 className="text-2xl ">Title</h1>
-                        <p className="text-sm sm:text-base text-left max-w-md sm:max-w-lg lg:max-w-none">
+                        <p className="text-[0.8rem] lg:text-base text-left max-w-md sm:max-w-lg lg:max-w-none">
                             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                             Esse cumque vitae labore sint, officia iusto ipsam, ut quidem harum veniam minus deleniti
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit...
