ef65800c7781f9dbaf405983d0b6f057a359169d
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-03-16 18:10:06 -0300

    Update NavSocialMedia.tsx

--- src/components/NavSocialMedia.tsx
+++ src/components/NavSocialMedia.tsx
@@ -8,8 +8,8 @@ import Link from 'next/link'
 
 const NavSocialMedia = () => {
     return (
-        <div className='w-40 h-12 bg-gray-400 rounded-xl flex items-center justify-around'>
-                <Link href={'/'} className='hover:bg-black duration-300 rounded p-2'>
+        <div className='w-44 h-12 bg-neutral-800 rounded-xl flex items-center justify-around'>
+                <Link href={'/'} className='hover:bg-[#0866FF] duration-300 rounded p-2'>
                     <Image
                         src={FacebookIcon}
                         alt='facebook home icon'
@@ -18,7 +18,7 @@ const NavSocialMedia = () => {
                 <span className='opacity-50'>
                     |
                 </span>
-                <Link href={'/'} className='hover:bg-black duration-300 rounded p-2'>
+                <Link href={'/'} className='hover:bg-gray-700 duration-300 rounded p-2'>
                     <Image
                         src={TwitterIcon}
                         alt='facebook home icon'
@@ -27,7 +27,7 @@ const NavSocialMedia = () => {
                 <span className='opacity-50'>
                     |
                 </span>
-                <Link href={'/'} className='hover:bg-black duration-300 rounded p-2'>
+                <Link href={'/'} className='hover:bg-[#0A66C2] duration-300 rounded p-2'>
                     <Image
                         src={LinkedinIcon}
                         alt='facebook home icon'
