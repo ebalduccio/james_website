3e0fc6b073f86da5ac0908bd129ea8c4a41780ed
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-04-11 12:36:28 -0300

    Update DashboardSideBar.tsx

--- src/components/DashboardSideBar.tsx
+++ src/components/DashboardSideBar.tsx
@@ -1,35 +1,27 @@
-import { NavItems } from '@/constants';
+import { SIDENAV_ITEMS } from '@/constants/SIDENAV_CONSTANT';
 import Link from 'next/link';
 import React from 'react'
 
 function DashboardSideBar() {
   return (
-    <aside className='fixed bg-neutral-600 z-50 w-[20rem] h-full text-white'>
-      <div className='flex flex-col items-center justify-center'>
-        <h1 className='text-5xl pt-10'>
-          JK
-        </h1>
-        <div>
-          <div>
-            All Posts
-          </div>
-          <div>
-            New Post
-          </div>
+    <>
+      <aside className='fixed bg-neutral-600 z-50 w-[20rem] h-full text-white'>
+        <div className='flex flex-col items-center justify-center'>
+          <h1 className='text-5xl pt-10'>
+            JK
+          </h1>
         </div>
-      </div>
-      <div className='flex flex-col gap-6'>
-        {NavItems.map((item, index) => {
-          return (
-            <Link key={index} href={item.href}>
-              <div className='p-2 m-2 hover:bg-zinc-500 w-auto duration-300 rounded'>
-                {item.label}
-              </div>
-            </Link>
-          );
-        })}
-      </div>
-    </aside>
+        <div className='flex flex-col gap-6 w-48 ml-4 mt-4 transition duration-300'>
+          {SIDENAV_ITEMS.map((item, index) => {
+            return (
+              < Link href={item.href} key={item.title} className='w-full h-10 rounded-sm px-4 duration-300 hover:bg-slate-200 hover:text-black font-normal flex items-center' >
+                {item.title}
+              </Link>
+            )
+          })}
+        </div>
+      </aside >
+    </>
   )
 }
 
