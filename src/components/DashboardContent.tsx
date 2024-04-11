823666269a36e457c7a07b1bca584fd9bea8cb50
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-04-11 01:51:18 -0300

    Create DashboardContent.tsx

---
+++ src/components/DashboardContent.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from 'react'
+
+function DashboardContent({ children }: {children: ReactNode}) {
+    return (
+        <div>DashboardContent</div>
+    )
+}
+
+export default DashboardContent
