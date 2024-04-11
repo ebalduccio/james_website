c13ac1e063a956516c0b0f1a6eb649bca548e66a
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-04-11 12:36:21 -0300

    Update DashboardContent.tsx

--- src/components/DashboardContent.tsx
+++ src/components/DashboardContent.tsx
@@ -2,7 +2,9 @@ import React, { ReactNode } from 'react'
 
 function DashboardContent({ children }: {children: ReactNode}) {
     return (
-        <div>DashboardContent</div>
+        <div className='flex-grow p-2 mt-20 text-black pl-[20.4rem]'>
+            {children}
+        </div>
     )
 }
 
