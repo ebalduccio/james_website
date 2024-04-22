5eecc1dc3768c5180230472bdbb7751ffb7f750d
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-04-22 18:21:56 -0300

    Create Leftbar.tsx

---
+++ src/app/personalblog/components/Leftbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import SidebarMenuItems from './SidebarMenuItems'
+import { SIDEBARPB } from '@/constants/SIDEBARPB_CONSTANT'
+
+const Leftbar = () => {
+    return (
+        <aside className='w-[15rem] h-[40rem]'>
+                <div className='flex flex-col gap-6 w-48 ml-4 mt-4 transition duration-300'>
+                    {SIDEBARPB.map((item, index) => {
+                        return (
+                            <SidebarMenuItems item={item} />
+                        )
+                    })}
+                </div>
+        </aside>
+    )
+}
+
+export default Leftbar
