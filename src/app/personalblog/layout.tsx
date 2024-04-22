765598a7572763b94deff29dd13c3938415217d9
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-04-22 18:22:05 -0300

    Update layout.tsx

--- src/app/personalblog/layout.tsx
+++ src/app/personalblog/layout.tsx
@@ -3,6 +3,11 @@ import '../globals.css'
 import { cn } from '@/lib/utils'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import RightbarPB from './components/Rightbar'
+import BlogContent from './components/BlogContent'
+import Leftbar from './components/Leftbar'
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import SectionTitle from '@/components/SectionTitle'
 
 export const metadata = {
   title: 'Personal Blog',
@@ -17,11 +22,19 @@ export default function RootLayout({
   return (
     <html lang="en" className='h-full '>
       <body className={cn('relative h-full antialiased font-roboto')}>
-        <main className='absolute sm:relative flex flex-col min-h-screen'>
+        <main className='absolute sm:relative bg-whitebg bg-center bg-no-repeat flex flex-col min-h-screen'>
           <div className='flex-grow flex-1'>
             <Navbar />
-            {children}
-            <Footer />
+            <SectionTitle TextColor='black' VectorColor='Black' title='Personal Blog' />
+            <MaxWidthWrapper className='items-center justify-center'>
+              <div className='flex pt-10'>
+                <Leftbar />
+                <BlogContent>
+                  {children}
+                </BlogContent>
+                <RightbarPB />
+              </div>
+            </MaxWidthWrapper>
           </div>
         </main>
       </body>
