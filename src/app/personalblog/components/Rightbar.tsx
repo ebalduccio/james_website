3a18718b1df0df025d2ff0d6722379c305aa808e
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-04-22 18:22:00 -0300

    Create Rightbar.tsx

---
+++ src/app/personalblog/components/Rightbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SidebarPB = () => {
+  return (
+    <div className='bg-blue-400 w-[15rem] h-full'>
+          sidebar
+    </div>
+  )
+}
+
+export default SidebarPB
