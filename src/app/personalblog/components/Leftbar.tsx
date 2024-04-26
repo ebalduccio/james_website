13da6856db43f00f073490a4ca9c83c6b674a28f
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-04-26 11:58:23 -0300

    Update Leftbar.tsx

--- src/app/personalblog/components/Leftbar.tsx
+++ src/app/personalblog/components/Leftbar.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import SidebarMenuItems from './SidebarMenuItems'
 import { SIDEBARPB } from '@/constants/SIDEBARPB_CONSTANT'
+import { cn } from '@/lib/utils'
 
-const Leftbar = () => {
+const Leftbar = ({ className }: { className?: string }) => {
     return (
-        <aside className='w-[15rem] h-[40rem]'>
-                <div className='flex flex-col gap-6 w-48 ml-4 mt-4 transition duration-300'>
-                    {SIDEBARPB.map((item, index) => {
-                        return (
-                            <SidebarMenuItems key={index} item={item} />
-                        )
-                    })}
-                </div>
+        <aside className={cn('w-[15rem] h-screen max-h-screen sticky top-[3rem]', className)}>
+            <div className='flex flex-col gap-6 w-48 ml-4 mt-4 transition duration-300'>
+                {SIDEBARPB.map((item, index) => {
+                    return (
+                        <SidebarMenuItems key={index} item={item} />
+                    )
+                })}
+            </div>
         </aside>
     )
 }
