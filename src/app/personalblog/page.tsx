81eeb02b7a79fa7cad608bc6f59a807c5014b0ef
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-04-26 11:58:33 -0300

    Update page.tsx

--- src/app/personalblog/page.tsx
+++ src/app/personalblog/page.tsx
@@ -1,9 +1,17 @@
+import PBPost from "./components/PBPost";
 
 export default function PersonalBlog() {
     return (
         <>
-            <div className="bg-red-400">
-                content
+            <div className="h-full">
+                <div className="grid grid-cols-1 h-full gap-4 items-center justify-center py-4 px-2">
+                    <PBPost />
+                    <PBPost />
+                    <PBPost />
+                    <PBPost />
+                    <PBPost />
+                    <PBPost />
+                </div>
             </div>
         </>
     )
