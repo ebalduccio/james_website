651c648fa49a6ca864418e7f3f9facbd6e5f2051
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-04-26 11:58:25 -0300

    Create PBPost.tsx

---
+++ src/app/personalblog/components/PBPost.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const PBPost = () => {
+    return (
+        <div className='h-[12rem] flex-grow rounded transition duration-250 border-b hover:bg-slate-300'>
+            <div className='px-3 py-4'>
+                <div className='flex flex-col'>
+                    <span className='text-lg font-bold'>Title lorem ipsum etc</span>
+                    <span className='text-xs font-medium'>8:36 A.M / APRIL 24</span>
+                </div>
+                <p className='flex items-center text-sm pt-4 '>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem iure unde consectetur excepturi!
+                    Deleniti mollitia, similique saepe quisquam animi magni est aut libero? Et eligendi libero quasi ea ut reiciendis!
+                    Deleniti mollitia, similique saepe quisquam animi magni est aut libero? Et eligendi libero quasi ea ut reiciendis!
+                    Deleniti mollitia, similique saepe quisquam animi magni est aut libero? Et eligendi libero quasi ea ut reiciendis!
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default PBPost
