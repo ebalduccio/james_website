af77d74de1ff8db8082ecd31446f8f658f3d5b14
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-05-02 11:22:25 -0300

    Update Rightbar.tsx

--- src/app/personalblog/components/Rightbar.tsx
+++ src/app/personalblog/components/Rightbar.tsx
@@ -1,10 +1,15 @@
+import { Input } from '@/components/ui/input'
+import { Search } from 'lucide-react'
 import React from 'react'
 
 const SidebarPB = () => {
   return (
-    <div className='bg-blue-400 w-[15rem] h-full'>
-          sidebar
-    </div>
+    <div className='w-full h-full'>
+      <div className="relative">
+        <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+        <Input placeholder="Search" className="pl-8" />
+      </div>
+    </div >
   )
 }
 
