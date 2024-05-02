93d994406a447ae0a31636b6047d2054f01cf79a
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-05-02 11:22:34 -0300

    Update Leftbar.tsx

--- src/app/personalblog/components/Leftbar.tsx
+++ src/app/personalblog/components/Leftbar.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils'
 
 const Leftbar = ({ className }: { className?: string }) => {
     return (
-        <aside className={cn('w-[15rem] h-screen max-h-screen sticky top-[3rem]', className)}>
+        <aside className={cn('w-[15rem] h-screen max-h-screen sticky hidden lg:block top-[3rem]', className)}>
             <div className='flex flex-col gap-6 w-48 ml-4 mt-4 transition duration-300'>
                 {SIDEBARPB.map((item, index) => {
                     return (
